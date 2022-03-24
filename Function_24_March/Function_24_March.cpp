586584e43951634569c0e99b4e0e1cde3ef8e807
Glib Petrachkov <petrasha@gmail.com>
2022-03-24 21:54:59 +0200

    Calc

--- Function_24_March/Function_24_March.cpp
+++ Function_24_March/Function_24_March.cpp
@@ -14,26 +14,25 @@
 
 #include <iostream>
 using namespace std;
-
-
-
-// пример функции, которая принимает 2 параметра
+// пример функции, которая принимает 3 параметра
 void Calculator(double first, char act, double second)
 {
-    //first = 5;
-    //act = 42;
-    //second = 3;
-   //cin >> first >> act >> second;
-    //cout << first << act << second << "\n";
-    double plus = first + second;
-    double minus = first - second;
-    double mult = first * second;
-    double div = first / second;
-    //char plus = 43;
-    //char mult = 42;
-    //char minus = 45;
-    //char div = 47;
-    cout << "result " << mult << "\n";
+    double result=0;
+    //double plus = first + second;
+    //double minus = first - second;
+    //double mult = first * second;
+    //double div = first / second;
+    //cin >> first >> act >> second;
+    if (act == 1) result = first + second;
+    else if (act == 2) result = first - second;
+    else if (act == 3) result = first * second;
+    else if (act == 4) result = first / second;
+    cout << result;
+    //char plu = 43;
+    //char mul = 42;
+    //char minu = 45;
+    //char di = 47;
+    
 }
 
 
@@ -42,12 +41,7 @@ int main()
 {
     double a, c;
     char b;
-    cin >> a >> b >> c;
+    cin >> a >> b >>  c;
     Calculator(a, b, c);
+
     
-   // double a, b;
-   // cin >> a >> b;
-    //Calculator(a, b); // передача 2 параметров
-    
-    cout << "Hello";
-}
