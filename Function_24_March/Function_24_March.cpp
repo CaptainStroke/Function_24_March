81098b562afb7ae785dd48dcca4cb5e3ce947651
Glib Petrachkov <petrasha@gmail.com>
2022-03-24 21:58:17 +0200

    calc

--- Function_24_March/Function_24_March.cpp
+++ Function_24_March/Function_24_March.cpp
@@ -15,33 +15,23 @@
 #include <iostream>
 using namespace std;
 // пример функции, которая принимает 3 параметра
-void Calculator(double first, char act, double second)
+void Calculator(double first, double act, double second)
 {
     double result=0;
-    //double plus = first + second;
-    //double minus = first - second;
-    //double mult = first * second;
-    //double div = first / second;
-    //cin >> first >> act >> second;
     if (act == 1) result = first + second;
     else if (act == 2) result = first - second;
     else if (act == 3) result = first * second;
     else if (act == 4) result = first / second;
-    cout << result;
-    //char plu = 43;
-    //char mul = 42;
-    //char minu = 45;
-    //char di = 47;
-    
+    cout << result;   
 }
 
 
 
 int main()
 {
-    double a, c;
-    char b;
-    cin >> a >> b >>  c;
+    double a, b, c;
+    //char b;
+    cin >> a >> b >> c;
     Calculator(a, b, c);
-
+}
     
